f1c4ea6a3ce822120cdae7fa53fb2a1d8946d01e
Laura Tannahill <lntannahill@gmail.com>
2020-12-29 10:05:27 -0400

    Initial commit - production branch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './laura.png';
 import './App.css';
 
 function App() {
@@ -6,17 +6,27 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+        Hi, I'm Laura! A <strong>Full-Stack Web Developer</strong> based in Halifax, NS.
+        </h1>
+        <h3>Under Construction!</h3>
+          <p>
+            In the meantime, check out a few of my projects:
+          </p>
+          <ul>
+            <li>
+              <p><a href="https://scheduler-laurtann.netlify.app/">Scheduler</a> An Interview Scheduler App</p>
+            </li>
+            <li>
+              <p><a href="https://tweeter-laurtann.herokuapp.com/">Tweeter</a> A Twitter Clone</p>
+            </li>
+            <li>
+              <p><a href="https://tinyapp-laurtann.herokuapp.com/urls">Tinyapp</a> A URL Shortener</p>
+            </li>
+            <li>
+              <p><a href="http://www.lauratannahill.com/robofriends/">Robofriends</a> A Robot Search App</p>
+            </li>
+          </ul>
       </header>
     </div>
   );
