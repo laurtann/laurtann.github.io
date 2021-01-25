5f4737729358baeef7a9e10cd5dc1a72cd34c349
Laura Tannahill <lntannahill@gmail.com>
2021-01-25 19:14:50 -0400

    Add new project

--- src/Projects.js
+++ src/Projects.js
@@ -2,11 +2,11 @@ export const cards = [
   {
     id: 6,
     name: "Layers",
-    imgSrc: "https://github.com/laurtann/laurtann.github.io/blob/build/public/docs/scheduler.png?raw=true",
-    description: `An Interview Scheduler App where a user can book and manage an interview with an instructor.
-    Tech Stack: ReactJS, PostgreSQL, Axios, WebSockets, Storybook, Jest & Cypress`,
-    repo: "https://github.com/laurtann/scheduler",
-    live: "https://scheduler-laurtann.netlify.app/"
+    imgSrc: "https://github.com/laurtann/laurtann.github.io/blob/build/public/docs/layers.png?raw=true",
+    description: `An app for music teachers and learners where users can isolate individual parts (stems) of their favourite songs and play or mute one or many.
+    Tech Stack: React, JavaScript, PostgreSQL, Node, Express, WaveSurfer & Material UI.`,
+    repo: "https://github.com/laurtann/LHL-final-project",
+    live: "https://layers-irl.netlify.app/"
   },
   {
     id: 1,
