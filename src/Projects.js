854e0a73a1e344e544aad4c92bf14dd118815b37
Laura Tannahill <lntannahill@gmail.com>
2021-01-27 12:28:36 -0400

    Add new text field for stack

--- src/Projects.js
+++ src/Projects.js
@@ -4,7 +4,8 @@ export const cards = [
     name: "Layers",
     imgSrc: "https://github.com/laurtann/laurtann.github.io/blob/build/public/docs/layers.png?raw=true",
     description: `An app for music teachers and learners where users can isolate individual parts (stems) of their favourite songs and play or mute one or many.
-    Tech Stack: React, JavaScript, PostgreSQL, Node, Express, WaveSurfer & Material UI. Demo login: email: mail@gmail.com, pw: password`,
+    Demo login: email: mail@gmail.com, pw: password`,
+    techStack: `Tech Stack: React, JavaScript, PostgreSQL, Node, Express, WaveSurfer & Material UI. `,
     repo: "https://github.com/laurtann/LHL-final-project",
     live: "https://layers-irl.netlify.app/"
   },
@@ -12,8 +13,8 @@ export const cards = [
     id: 1,
     name: "Scheduler",
     imgSrc: "https://github.com/laurtann/laurtann.github.io/blob/build/public/docs/scheduler.png?raw=true",
-    description: `An Interview Scheduler App where a user can book and manage an interview with an instructor.
-    Tech Stack: ReactJS, PostgreSQL, Axios, WebSockets, Storybook, Jest & Cypress`,
+    description: `An Interview Scheduler App where a user can book and manage an interview with an instructor.`,
+    techStack: `Tech Stack: ReactJS, PostgreSQL, Axios, WebSockets, Storybook, Jest & Cypress`,
     repo: "https://github.com/laurtann/scheduler",
     live: "https://scheduler-laurtann.netlify.app/"
   },
@@ -21,8 +22,8 @@ export const cards = [
     id: 2,
     name: "Tweeter",
     imgSrc: "https://github.com/laurtann/laurtann.github.io/blob/build/public/docs/tweeter.png?raw=true",
-    description: `A simple Twitter clone.
-    Tech Stack: Express, jQuery, JS & EJS.`,
+    description: `A simple Twitter clone.`,
+    techStack: `Tech Stack: Express, jQuery, JS & EJS.`,
     repo: "https://github.com/laurtann/tweeter",
     live: "https://tweeter-laurtann.herokuapp.com/"
   },
@@ -30,8 +31,8 @@ export const cards = [
     id: 3,
     name: "Mood4Food",
     imgSrc: "https://github.com/laurtann/laurtann.github.io/blob/build/public/docs/mood4food.png?raw=true",
-    description: `A food ordering app (team project). After an order is placed, the restaurant & user are updated of order status via text message and preparation time is dynamically rendered on the confirmation page *Text functionality not available on deployed version.
-    Tech Stack: Twilio, Express, JS, EJS, PostgreSQL, Bootstrap & jQuery.`,
+    description: `A food ordering app (team project). After an order is placed, the restaurant & user are updated of order status via text message and preparation time is dynamically rendered on the confirmation page *Text functionality not available on deployed version.`,
+    techStack: `Tech Stack: Twilio, Express, JS, EJS, PostgreSQL, Bootstrap & jQuery.`,
     repo: "https://github.com/laurtann/mood4food",
     live: "https://mood4food-laurtann.herokuapp.com/"
   },
@@ -39,7 +40,8 @@ export const cards = [
     id: 4,
     name: "Tinyapp",
     imgSrc: "https://github.com/laurtann/laurtann.github.io/blob/build/public/docs/tinyapp.png?raw=true",
-    description: `A URL shortening app. Tech Stack: Express, JS, jQuery, Ajax, Bootstrap & EJS.`,
+    description: `A URL shortening app.`,
+    techStack: `Tech Stack: Express, JS, jQuery, Ajax, Bootstrap & EJS.`,
     repo: "https://github.com/laurtann/tinyapp",
     live: "https://tinyapp-laurtann.herokuapp.com/urls"
   },
@@ -47,8 +49,8 @@ export const cards = [
     id: 5,
     name: "Robofriends",
     imgSrc: "https://github.com/laurtann/laurtann.github.io/blob/build/public/docs/robofriends.png?raw=true",
-    description: `Search through our robot directory! A simple React app built while completing Andrei Neagoie's Udemy course.
-    Tech Stack: React, JS & Tachyons`,
+    description: `Search through our robot directory! A simple React app built while completing Andrei Neagoie's Udemy course.`,
+    techStack: `Tech Stack: React, JS & Tachyons`,
     repo: "https://github.com/laurtann/robofriends",
     live: "https://www.lauratannahill.com/robofriends"
   }
